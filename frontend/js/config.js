06cfbcaf502eabb167e5ba07e736e55b687fe06e
LeonardIfiii <l.marx@ifii.eu>
2026-02-26 23:04:53 +0100

    gitignore update

---
+++ frontend/js/config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  SUPABASE_URL:  'https://ykepizruwftkimxuycpg.supabase.co',
+  SUPABASE_ANON: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrZXBpenJ1d2Z0a2lteHV5Y3BnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMjMwMTUsImV4cCI6MjA4NzY5OTAxNX0.9NkXNIiwx-iSSrodSfobPDgIyOglfU6ePw_pE-9vhqU',
+};
